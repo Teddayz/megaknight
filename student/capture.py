a7d2c330599411df31df885a976704e5c9a4d3a2
Teddayz <mervynneo02@gmail.com>
2025-11-22 14:33:29 -0800

    Change to Offensive and Defensive Agent

--- student/capture.py
+++ student/capture.py
@@ -5,8 +5,7 @@ def create_team() -> list[pacai.core.agentinfo.AgentInfo]:
     """
     Get the agent information that will be used to create a capture team.
     """
-
-    agent1_info = pacai.core.agentinfo.AgentInfo(name = pacai.util.alias.AGENT_DUMMY.long)
-    agent2_info = pacai.core.agentinfo.AgentInfo(name = pacai.util.alias.AGENT_DUMMY.long)
+    agent1_info = pacai.core.agentinfo.AgentInfo(name = pacai.util.alias.AGENT_CAPTURE_DEFENSIVE)
+    agent2_info = pacai.core.agentinfo.AgentInfo(name = pacai.util.alias.AGENT_CAPTURE_OFFENSIVE)
 
     return [agent1_info, agent2_info]
